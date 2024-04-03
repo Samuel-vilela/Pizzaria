6bd20496d0cdc7aa6edf46ba633d8627d804a136
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-04-02 21:57:10 -0300

    pizzaria

---
+++ pizzaria/contato.php
@@ -0,0 +1,33 @@
+<?php include ("cabecalho.php")?>
+<?php include ("menu.php")?>
+    <div class="row bg-light">
+        <div class="col">
+            <h1>Entre em contato</h1>
+            <hr>
+            <div class="row">
+                <div class="col">
+                <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <label for="exampleFormControlInput1" class="form-label">E-mail</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <label for="exampleFormControlInput1" class="form-label">Assunto</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        </div>
+                        <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Mensagem</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        </form>
+                        <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>                            
+                </div>
+                <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7359.841166034125!2d-47.38420065708282!3d-22.731193017515924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89c7c85f71aa5%3A0xfe1375059b5bd130!2sParque%20Res.%20Zabani%2C%20Santa%20B%C3%A1rbara%20d&#39;Oeste%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1712096810421!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include ("footer.php")?>
